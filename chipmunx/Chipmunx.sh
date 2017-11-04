7499149fdcc78c729758a0946d4d24e57abb8ad8
Hillaby <silvestre.toth@gmail.com>
2017-11-04 16:06:16 +0100

    --silent option

--- chipmunx/Chipmunx.sh
+++ chipmunx/Chipmunx.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+# --silent - remove it if you have enough CPU
+
 chuck --srate44100 \
-../lib/timer.ck \
-../lib/rec.ck \
-../lib/rand_loop.ck \
-../lib/stereo_rand_loop.ck \
-chipmunx.ck
+    --silent \
+    ../lib/timer.ck \
+    ../lib/rec.ck \
+    ../lib/rand_loop.ck \
+    ../lib/stereo_rand_loop.ck \
+    chipmunx.ck
--- chipmunx/README.md
+++ chipmunx/README.md
@@ -1,2 +1,5 @@
 To listen to this song, just run `Chipmunx.sh`.
 
+(Currently this will generate the left and right tracks, but will not
+emit a sound. If you have enough CPU, you can remove the `--silent` option.
+To combine the two tracks you can use Audacity.)
